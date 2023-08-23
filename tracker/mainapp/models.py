44964f1253686ffd3ef6d0c1d0cef990997afc27
arshjit pelia <arsh.pelia1@gmail.com>
2023-08-23 15:41:03 -0400

    added models for question, progress and note

--- tracker/mainapp/models.py
+++ tracker/mainapp/models.py
@@ -1,7 +1,42 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 class User(AbstractUser):
-    # inherits from AbstractUser, it will already have fields for a username, email, password
+    # Any custom fields you might want to add to the user model
     pass
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    number = models.PositiveIntegerField(unique=True)
+    description = models.TextField()
+    DIFFICULTY_CHOICES = [
+        ('easy', 'Easy'),
+        ('medium', 'Medium'),
+        ('hard', 'Hard'),
+    ]
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
+    tags = models.CharField(max_length=200)
+    first_attempt = models.DateTimeField(auto_now_add=True)
+    last_attempt = models.DateTimeField(auto_now=True)
+    solved_first_time = models.BooleanField()
+    attempts = models.PositiveIntegerField(default=1)
+
+class Note(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Progress(models.Model):
+    STATUS_CHOICES = [
+        ('solved', 'Solved'),
+        ('unsolved', 'Unsolved'),
+        ('in_progress', 'In Progress'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    completion_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('user', 'question')
